f73542b364f526fe21008649d3decd2861b1256f
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-07-30 07:59:50 -0700

    Insertion sort using a for loop

--- sorting/InsertionSort.scala
+++ sorting/InsertionSort.scala
@@ -1,4 +1,6 @@
 package sorting
+
+import scala.util.control.Breaks._
 // To compile while in the root folder scalac sorting/InsertionSort.scala
 // To execute scala sorting.InsertionSort
 // It prints out something like: Sorted List -> List(1, 2, 3, 4, 5, 6)
@@ -23,7 +25,27 @@ object InsertionSort extends App {
       }
     }
 
-    val list = List(5,2,4,6,1,3)
-    val sortedList = insertionSort(list)
+    def insertionSortLoop(list :List[Int]) : List[Int] = {
+      val intArray: Array[Int] = list.toArray
+      for ( j <- 1 until intArray.length ) {
+          breakable {
+            for ( i <- (1 to j).reverse ) {
+              if (intArray(i-1) < intArray(i)) {
+                break
+              } else {
+                  val temp = intArray(i)
+                  intArray(i) = intArray(i-1)
+                  intArray(i-1) = temp
+              }
+            }
+        }
+      }
+      intArray.toList
+    }
+
+    var list = List(5,2,4,6,1,3)
+    var sortedList = insertionSort(list)
     println("Sorted List -> " + sortedList.toString() )
+    sortedList = insertionSortLoop(list)
+    println("Sorted List Loop -> " + sortedList.toString() )
 }
